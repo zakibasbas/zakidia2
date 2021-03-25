35377deaa098609878e42bd90b4e6f476222c5cd
Dia Derradji <rnoir24@gmail.com>
2021-03-25 21:22:39 +0100

    develope calculator

--- src/tp74/calc.java
+++ src/tp74/calc.java
@@ -1,52 +1,178 @@
-package tp74;
+ package tp74;
 
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 		 
 		// An AWT GUI program inherits the top-level container java.awt.Frame
 		public class calc extends Frame {
-		   private Button[] btn;  // Array of 10 numeric Buttons
-		   private Button btnHash, btnStar;
-		   private TextField tfDisplay;
-		 
+		private Button[] btn;  // Array of 10 numeric Buttons
+		   public TextField tfDisplay,rsDisplay;
+		   public Label tfLabel,rsLabel;
+		   private float Result=0;
+		   private char optTex=' ';
+		   private String Tex,rTex="";
 		   // Constructor to setup GUI components and event handlers
 		   public calc () {
 		      // Set up display panel
-		      Panel panelDisplay = new Panel(new FlowLayout());
-		      tfDisplay = new TextField("0", 20);
+		      Panel panelDisplay = new Panel(new GridLayout(2, 2) );
+		      Tex="0";
+		      tfDisplay = new TextField(Tex, 20);
+		      rsDisplay = new TextField(rTex, 20);
+		      tfLabel=new Label("Operation");
+		      rsLabel=new Label("Result");
+		      panelDisplay.add(tfLabel);
 		      panelDisplay.add(tfDisplay);
-		 
+		      panelDisplay.add(rsLabel);
+		      panelDisplay.add(rsDisplay);
 		      // Set up button panel
-		      Panel panelButtons = new Panel(new GridLayout(4, 3)); // 4 = lignes , 3= col 
-		      btn = new Button[10];  // Construct an array of 10 numeric Buttons
-		      
-		      btn[1] = new Button("1");  // Construct Button "1"
-		      panelButtons.add(btn[1]);  // The Panel adds this Button
-		     
-		      btn[2] = new Button("2");
-		      panelButtons.add(btn[2]);
-		      
-		      btn[3] = new Button("3");
-		      panelButtons.add(btn[3]);
-		      
+		      Panel panelButtons = new Panel(new GridLayout(4, 4));
 		
-		            // You should use a loop for the above statements!!!
-		      btnStar = new Button("*");
-		      panelButtons.add(btnStar);
-		      
-		      btn[0] = new Button("0");
-		      panelButtons.add(btn[0]);
-		      
-		      btnHash = new Button("#");
-		      panelButtons.add(btnHash);
+		      btn = new Button[10]; 
+		      int j=0;// Construct an array of 10 numeric Buttons
+		      for(int i=0;i<16;i++)
+		     {  switch(i) {
+		     case 3 : {  Button btnPlus = new Button("+"); 
+		     panelButtons.add( btnPlus);
+		
+		     btnPlus.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if (optTex==' ')
+						 {optTex='+';
+						 Result=Float.parseFloat(Tex);
+						 Tex+=optTex;
+						 rsDisplay.setText(String.valueOf(Result));
+						 tfDisplay.setText(Tex );}
+						}}); 
+		         
+		     }break;
+		     case 7: {  Button btnMinus = new Button("-"); 
+		     panelButtons.add(btnMinus);
+		     
+		     btnMinus.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if (optTex==' ')
+						 {optTex='-';
+						 Result=Float.parseFloat(Tex);
+						 Tex+=optTex;
+						 rsDisplay.setText(String.valueOf(Result));
+						 tfDisplay.setText(Tex );}
+						}});
+		    
+		     }break;
+		     case 11: { Button btnMultiply = new Button("*"); 
+		     panelButtons.add(btnMultiply);
+		     
+		     btnMultiply.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if (optTex==' ')
+						 {optTex='*';
+						 Result=Float.parseFloat(Tex);
+						 Tex+=optTex;
+						 rsDisplay.setText(String.valueOf(Result));
+						 tfDisplay.setText(Tex );
+						 }	}});
+		    
+		     }break;     
+		     case 12: {  Button btnDot = new Button("."); 
+		     panelButtons.add(  btnDot);
+		     }break;
+		     case 13: { btn [9] = new Button("0"); 
+		      btn[9].addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if (Tex=="0")
+						Tex=e.getActionCommand();
+						else Tex+=e.getActionCommand();
+						tfDisplay.setText(Tex ); 
+					if(Result!=0)
+						rTex+=e.getActionCommand();
+			//		
+						
+					}});
+		     panelButtons.add(btn [9]);
+		     }break;
+		     case 14: { Button btnEqual  = new Button("="); 
+		     panelButtons.add(btnEqual );
+		     
+		     btnEqual .addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						  switch(optTex) {
+						  case '+' :if (Result!=0)
+							  Result+=Integer.parseInt(rTex);
+							  break;  
+							  case '*' :if (Result!=0)
+								  Result*=Integer.parseInt(rTex); break;  
+								  case '-' :if (Result!=0)
+									  Result-=Integer.parseInt(rTex); break;  
+									  case '/' :if (Result!=0)
+										  Result/=Integer.parseInt(rTex); break;  
+						 }
+						  rsDisplay.setText(String.valueOf(Result));
+						  optTex=' ';
+						  Tex="0";
+						  tfDisplay.setText(Tex );
+						  Result=0;
+						  rTex="";
+					}});
+		     
+		     
+		    	 
+		     }break;     
+		     case 15: { Button btnDivide  = new Button("/"); 
+		     panelButtons.add(btnDivide );
+		     
+		     btnDivide.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if (optTex==' ')
+						 { optTex='/';
+						 Result=Float.parseFloat(Tex);
+						 Tex+=optTex;
+						 rsDisplay.setText(String.valueOf(Result));
+						 tfDisplay.setText(Tex );
+						 }
+					}});
 		 
+		     }break; 
+		     default:{ btn[j] = new Button(String.valueOf(j+1)); 
+		      btn[j].addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if (Tex=="0")
+					Tex=e.getActionCommand();
+					else Tex+=e.getActionCommand();
+					tfDisplay.setText(Tex ); 
+				if(Result!=0)
+					rTex+=e.getActionCommand();
+		//		
+					
+				}});
+		      panelButtons.add(btn[j]); 
+		     j++;	 
+		     }; }  }
+		 
+		          
 		      setLayout(new BorderLayout());  // "super" Frame sets to BorderLayout
 		      add(panelDisplay, BorderLayout.NORTH);
 		      add(panelButtons, BorderLayout.CENTER);
 		 
 		      setTitle("caculatrice"); // "super" Frame sets title
-		      setSize(250, 250);  // la taille de l'afichage           // "super" Frame sets initial size
-		      setVisible(true);              // "super" Frame shows
+		      setSize(300, 300);  // la taille de l'afichage           // "super" Frame sets initial size
+		      setVisible(true);     
+		      // "super" Frame shows
 		   }
 		 
 		   // The entry main() method
